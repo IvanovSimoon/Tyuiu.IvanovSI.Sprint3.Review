2933ee7800c85c9a673ea21382f4e3a3437aa21c
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-12-02 11:10:53 +0300

    Review

---
+++ Tyuiu.IvanovSI.Sprint3.Review.V4.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.IvanovSI.Sprint3.Review.V4.Lib
+{
+    public class DataService : ISprint3Task7V4
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int cnt = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((((2 * x + 6) / (Math.Cos(x) + x)) - 3),2);
+
+                valueArray[cnt] = y;
+                cnt++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.IvanovSI.Sprint3.Review.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovSI.Sprint3.Review.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovSI.Sprint3.Review.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7964ae9a-323b-4da2-adb7-e042cc6f5057")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
